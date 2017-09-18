e6124748d0d81efea440a5e2605380c1aa4e68e7
JASH <Jacob.Shearer@au.saabgroup.com>
2017-09-18 13:54:54 +0930

    Add robot framework to build in

---
+++ src/gradbots/GradBot.java
@@ -0,0 +1,72 @@
+package gradbots;
+
+
+
+import java.awt.Color;
+
+import robocode.AdvancedRobot;
+import robocode.BulletHitBulletEvent;
+import robocode.BulletHitEvent;
+import robocode.BulletMissedEvent;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.RobotDeathEvent;
+import robocode.ScannedRobotEvent;
+import robocode.SkippedTurnEvent;
+import robocode.WinEvent;
+
+public class GradBot extends AdvancedRobot {
+	
+	public void run(){
+		setColors(Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE);		
+		while(true){
+			execute();
+		}
+	}
+	
+	@Override
+	public void onScannedRobot(ScannedRobotEvent event) {
+	}
+	
+	@Override
+	public void onHitWall(HitWallEvent event) {
+		System.out.println("WARNING: I hit a wall at " + event.getTime() 
+			+ " angle from me " + event.getBearing());
+	}
+	
+	@Override
+	public void onHitRobot(HitRobotEvent event) {
+	}
+	
+	@Override
+	public void onRobotDeath(RobotDeathEvent event) {
+	}
+	
+	@Override
+	public void onBulletHit(BulletHitEvent event) {
+	}
+	
+	@Override
+	public void onBulletHitBullet(BulletHitBulletEvent event) {
+	}
+	
+	@Override
+	public void onBulletMissed(BulletMissedEvent event) {
+	}
+	
+	@Override
+	public void onHitByBullet(HitByBulletEvent event) {
+	}
+	
+	@Override
+	public void onWin(WinEvent event) {
+	}
+	
+	@Override
+	public void onSkippedTurn(SkippedTurnEvent event) {
+		System.out.println("WARNING: Turn skipped at: " + event.getTime() 
+			+ ", turn " + event.getSkippedTurn());
+	}
+
+}
