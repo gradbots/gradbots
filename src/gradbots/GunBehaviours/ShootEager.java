b9801b9832199a23ae0e82dd914fa41aea25b882
JASH <Jacob.Shearer@au.saabgroup.com>
2017-10-03 15:28:48 +1030

    Add some base structure.

--- src/gradbots/GunBehaviours/ShootEager.java
+++ src/gradbots/GunBehaviours/ShootEager.java
@@ -1,5 +1,37 @@
 package gradbots.GunBehaviours;
 
-public class ShootEager {
+import java.util.ArrayList;
 
+import gradbots.GradBot;
+import gradbots.Interfaces.IGunBehaviour;
+import javafx.geometry.Point2D;
+import robocode.RobotDeathEvent;
+import robocode.ScannedRobotEvent;
+
+/**
+ * @author dxst and jash
+ *
+ */
+public class ShootEager extends GunBehaviourBase {
+
+	private boolean hasTarget = false;
+	private GradBot robot;
+	private ArrayList<Point2D> robotPosList = new ArrayList<Point2D>();
+	
+	public ShootEager(GradBot r) {
+		super(r);
+	}
+	
+	@Override
+	public void doDefault() {
+	}
+
+	@Override
+	public void onRobotDeath(RobotDeathEvent e) {
+	}
+
+	@Override
+	public void onScannedRobot(ScannedRobotEvent e) {
+	}
+	
 }
